71ff20cbf12e6699b2d309cbf9297d6010b3f145
kaynansn11@gmail.com <kaynansn11@gmail.com>
2023-08-21 10:14:18 -0300

    committing

--- mysite/blog/views/post_view.py
+++ mysite/blog/views/post_view.py
@@ -1,10 +1,11 @@
-from django.views import generic
+from django.views.generic import ListView, DetailView
 from blog.models import Post
 
-class PostView(generic.ListView):
+class PostView(ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = "index.html"
+    context_object_name = "posts"
 
-class PostDetail(generic.DetailView):
+class PostDetail(DetailView):
     model = Post
     template_name = 'post_detail.html'
